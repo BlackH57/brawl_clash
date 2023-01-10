2a1993185863df02796c99e677f20d815dcc278a
Enzo Roaldes <73364192+TrueDuc@users.noreply.github.com>
2023-01-10 17:24:13 +0100

    Update Entity.py

--- src/Entity.py
+++ src/Entity.py
@@ -3,13 +3,13 @@ from src import Bag
 
 class Entity:
 
-    def __init__(self, health: int, speed: int, coords: (int, int), bag_max: int, sprite: str):
+    def __init__(self, health: int, speed: int, x: int, y: int, bag_max: int, sprite: str):
         self.health = health
         self.speed = speed
         self.sprite = sprite    # En vrai doit contenir l'image
 
-        self.x = coords[0]
-        self.y = coords[1]
+        self.x = x
+        self.y = y
         self.bag = Bag.Bag(bag_max, [])
 
     def get_coord(self):
