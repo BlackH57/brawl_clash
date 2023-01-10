63c98669eb9e952f6f35ab7e797c48094d1b3c23
Enzo Roaldes <73364192+TrueDuc@users.noreply.github.com>
2023-01-10 12:58:03 +0100

    modif param + commentaires

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame as pg
 
+resolution = (1280, 800) #résolution de l'écran
+pg.init() 
 
-pg.init()
-screen = pg.DOUBLEBUF
+pg.display.set_caption("Brawl Clash") #titre de la fenêtre de jeu
 
+window_surface = pg.display.set_mode(resolution, pg.RESIZABLE) #ouverture de la fenêtre + modes
+
+#boucle pour garder la fenêtre ouverte tant qu'on appuie pas sur la croix
 launched = True
-
 while launched :
     for event in pg.event.get():
         if event.type == pg.QUIT:
